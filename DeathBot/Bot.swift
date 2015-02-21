a0913fb29854ecc5284f7e153fde71f4cf956488
Jenny <jenny.jieun.song@gmail.com>
2015-02-20 20:39:05 -0800

    started bot class

--- DeathBot/Bot.swift
+++ DeathBot/Bot.swift
@@ -1,9 +1,56 @@
-//
-//  Bot.swift
-//  DeathBot
-//
-//  Created by Younsuk Oh on 2/18/15.
-//  Copyright (c) 2015 SOJUniverse. All rights reserved.
-//
 
 import Foundation
+
+class Bot {
+    var month: Int
+    var gender: String
+    var location: String
+    var happiness: Int
+    var health: Int
+    var smoker: Int
+    var drinker: Int
+
+    
+    init(gender: String, location: String) {
+       
+        month = 1
+        self.gender = gender
+        self.location = location
+        happiness = Int(arc4random()) % 50 + 50
+        health = Int(arc4random()) % 50 + 50
+        smoker = 0
+        drinker = 0
+    }
+    
+    func take(food: String){
+        
+        switch food {
+            case "potato":
+                self.happiness += 5
+                self.health += 5
+
+            case "cigarette":
+                self.happiness += 5
+                self.health -= 5
+            
+            default:
+                break
+        
+        }
+        
+    }
+    
+    func doAction(action: String){
+        //do action.
+        
+        
+    }
+    
+}
+
+
+
+
+
+
+
---
+++ DeathBot/MyPlayground.playground/section-1.swift
@@ -0,0 +1,9 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var a = Int(arc4random()) % 50 + 50
+
+println(a)
+
