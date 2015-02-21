a729dd9fb56b46f2d6ddc884b651dcb3b8a17e49
Jenny <jenny.jieun.song@gmail.com>
2015-02-21 04:37:55 -0800

    :D

--- DeathBot/Bot.swift
+++ DeathBot/Bot.swift
@@ -9,10 +9,10 @@ class Bot {
     var health: Int
     var smoker: Int
     var drinker: Int
+    var status: String
 
     
     init(gender: String, location: String) {
-       
         month = 1
         self.gender = gender
         self.location = location
@@ -20,31 +20,103 @@ class Bot {
         health = Int(arc4random()) % 50 + 50
         smoker = 0
         drinker = 0
+        status = "toddler"
     }
     
+    
+    func grow_month(){
+        self.month++
+        if month >= 36 && month < 156 {
+            status = "child"
+        }
+        else if month >= 156 && month < 240 {
+            status = "teen"
+        }
+        else if month >= 240 && month < 660 {
+            status = "adult"
+        }
+        else if month >= 660 && month < 960 {
+            status = "senior"
+        }
+        else if month >= 960 {
+            status = "oldie"
+        }
+    }
+    
+    
     func take(food: String){
-        
         switch food {
-            case "potato":
+            case "icecream":
                 self.happiness += 5
                 self.health += 5
-
+            case "hamburger":
+                self.happiness += 5
+                self.health -= 5
+            case "pizza":
+                self.happiness += 5
+                self.health += 5
+            case "hamburger":
+                self.happiness += 5
+                self.health -= 5
+            case "donut":
+                self.happiness += 5
+                self.health += 5
+            case "milk":
+                self.happiness += 5
+                self.health -= 5
+            case "coffee":
+                self.happiness += 5
+                self.health += 5
             case "cigarette":
                 self.happiness += 5
                 self.health -= 5
-            
+            case "icecream":
+                self.happiness += 5
+                self.health += 5
+            case "hamburger":
+                self.happiness += 5
+                self.health -= 5
+            case "pizza":
+                self.happiness += 5
+                self.health += 5
+            case "hamburger":
+                self.happiness += 5
+                self.health -= 5
+            case "donut":
+                self.happiness += 5
+                self.health += 5
+            case "milk":
+                self.happiness += 5
+                self.health -= 5
+            case "coffee":
+                self.happiness += 5
+                self.health += 5
+            case "cigarette":
+                self.happiness += 5
+                self.health -= 5
+            case "donut":
+                self.happiness += 5
+                self.health += 5
+            case "milk":
+                self.happiness += 5
+                self.health -= 5
+            case "coffee":
+                self.happiness += 5
+                self.health += 5
+            case "cigarette":
+                self.happiness += 5
+                self.health -= 5
             default:
                 break
-        
         }
-        
     }
+    
     
     func doAction(action: String){
         //do action.
-        
-        
     }
+    
+    
     
 }
 
