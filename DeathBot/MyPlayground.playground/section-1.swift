e0ff3ee78fcb8905a6644efe980a9561396fc380
Jenny <jenny.jieun.song@gmail.com>
2015-02-21 09:53:15 -0800

    moving speed fixed & random food time

--- DeathBot/MyPlayground.playground/section-1.swift
+++ DeathBot/MyPlayground.playground/section-1.swift
@@ -6,17 +6,9 @@ var str = "Hello, playground"
 var a: Int = 119/12
 println(a)
 
+sqrt(9.5)
+pow(2.0, 3.0)
 
 
-arc4random()%3
-arc4random()%3
-arc4random()%3
-arc4random()%3
-arc4random()%3
-arc4random()%3
-arc4random()%3
-arc4random()%3
-arc4random()%3
-arc4random()%3
-arc4random()%3
-arc4random()%3
+
+
