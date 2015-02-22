87ddd2bffeeba1c35053b54f2e1adf5e5c2d97f1
Jenny <jenny.jieun.song@gmail.com>
2015-02-22 13:28:12 -0800

    added  bgm

--- DeathBot/Game.swift
+++ DeathBot/Game.swift
@@ -12,6 +12,30 @@ import SpriteKit
 import UIKit
 import AVFoundation
 
+var backgroundMusicPlayer: AVAudioPlayer!
+
+func playBackgroundMusic(filename: String) {
+    let url = NSBundle.mainBundle().URLForResource(
+        filename, withExtension: nil)
+    if (url == nil) {
+        println("Could not find file: \(filename)")
+        return
+    }
+    
+    var error: NSError? = nil
+    backgroundMusicPlayer =
+        AVAudioPlayer(contentsOfURL: url, error: &error)
+    if backgroundMusicPlayer == nil {
+        println("Could not create audio player: \(error!)")
+        return
+    }
+    
+    backgroundMusicPlayer.numberOfLoops = -1
+    backgroundMusicPlayer.prepareToPlay()
+    backgroundMusicPlayer.play()
+}
+
+
 
 class Game: SKScene {
     var botDataManager = BotDataManager()
@@ -22,7 +46,7 @@ class Game: SKScene {
         bot = botDataManager.get()
         super.init(size: size)
         
-        
+        playBackgroundMusic("humptydumpty.mp3")
         var background = SKSpriteNode(imageNamed: "landingBG")
         background.yScale = self.size.width/background.size.width
         background.size.width = self.size.width
